7d2321ad9d3b87cacc89ae3df58e678dcdc96264
Jacky Sun <jackysun@Jackys-MacBook-Air.local>
2016-03-04 15:52:32 +0800

    add bootstrap

--- app/mock-players.js
+++ app/mock-players.js
@@ -8,29 +8,59 @@ System.register([], function(exports_1, context_1) {
             //Typescript array: 1.var a:string[] 2. var a:Array<string>
             exports_1("PLAYERS", PLAYERS = [
                 { "id": 10, "name": "Lionel Messi", "chineseName": "梅西", "height": "169cm", "weight": "67kg",
-                    "birthday": "1987 06-24", "pic": ["public\\img\\meixi1.jpg", "public\\img\\meixi2.jpg"] },
+                    "birthday": "1987 06-24",
+                    "pic": ["public\\img\\meixi1.jpg", "public\\img\\meixi2.jpg", "public\\img\\meixi3.jpg"]
+                },
                 { "id": 9, "name": "Luis Alberto Suarez Díaz", "chineseName": "苏亚雷斯", "height": "181cm", "weight": "81kg",
-                    "birthday": "1987 01-24", "pic": ["public\\img\\suya1.jpg", "public\\img\\suya2.jpg"] },
+                    "birthday": "1987 01-24",
+                    "pic": ["public\\img\\suya1.jpg", "public\\img\\suya2.jpg", "public\\img\\suya3.jpg"]
+                },
                 { "id": 11, "name": "Neymar da Silva Santos Júnior", "chineseName": "内马尔", "height": "175cm", "weight": "64kg",
-                    "birthday": "1992 02-05", "pic": ["public\\img\\neimaer1.jpg", "public\\img\\neimaer2.jpg"] },
+                    "birthday": "1992 02-05",
+                    "pic": ["public\\img\\neimaer1.jpg", "public\\img\\neimaer2.jpg", "public\\img\\neimaer3.jpg"]
+                },
                 { "id": 1, "name": "Marc-André ter Stegen", "chineseName": "特尔", "height": "189cm", "weight": "88kg",
-                    "birthday": "1992 04-30", "pic": ["public\\img\\teller1.jpg", "public\\img\\meixi2.jpg"] },
+                    "birthday": "1992 04-30",
+                    "pic": ["public\\img\\teller1.jpg", "public\\img\\teller1.jpg", "public\\img\\teller1.jpg"]
+                },
                 { "id": 2, "name": "Douglas Pereira dos Santos", "chineseName": "道格拉斯", "height": "172cm", "weight": "63kg",
-                    "birthday": "1990 08-06", "pic": ["public\\img\\daoge1.jpg", "public\\img\\suya2.jpg"] },
+                    "birthday": "1990 08-06",
+                    "pic": ["public\\img\\daoge1.jpg", "public\\img\\daoge1.jpg", "public\\img\\daoge1.jpg"]
+                },
                 { "id": 3, "name": "Gerard Pique", "chineseName": "皮克", "height": "192cm", "weight": "85kg",
-                    "birthday": "1987 02-02", "pic": ["public\\img\\pke1.jpg", "public\\img\\neimaer2.jpg"] },
-                { "id": 4, "name": "Ivan Rakitic", "chineseName": "拉基蒂奇", "height": "180cm", "weight": "72kg",
-                    "birthday": "1988 03-10", "pic": ["public\\img\\yiwan1.jpg", "public\\img\\meixi2.jpg"] },
-                { "id": 5, "name": "Sergi Busquets Burgos", "chineseName": "布斯克茨", "height": "189cm", "weight": "73kg",
-                    "birthday": "1988 07-16", "pic": ["public\\img\\buzi1.jpg", "public\\img\\suya2.jpg"] },
-                { "id": 6, "name": "Dani Alves da Silva", "chineseName": "阿尔维斯", "height": "173cm", "weight": "64kg",
-                    "birthday": "1983 05-06", "pic": ["public\\img\\weisi1.jpg", "public\\img\\neimaer2.jpg"] },
-                { "id": 8, "name": "Andres Iniesta", "chineseName": "伊涅斯塔", "height": "170cm", "weight": "65kg",
-                    "birthday": "1984 05-11", "pic": ["public\\img\\yinie1.jpg", "public\\img\\meixi2.jpg"] },
-                { "id": 12, "name": "Rafael Alcantara do Nascimento", "chineseName": "拉菲尼亚", "height": "172cm", "weight": "63kg",
-                    "birthday": "1993 03-12", "pic": ["public\\img\\lafei1.jpg", "public\\img\\suya2.jpg"] },
-                { "id": 13, "name": "Claudio Bravo", "chineseName": "布拉沃", "height": "185cm", "weight": "83kg",
-                    "birthday": "1983 04-13", "pic": ["public\\img\\bulawo1.jpg", "public\\img\\neimaer2.jpg"] },
+                    "birthday": "1987 02-02",
+                    "pic": ["public\\img\\pke1.jpg", "public\\img\\pke1.jpg", "public\\img\\pke1.jpg"]
+                },
+                {
+                    "id": 4, "name": "Ivan Rakitic", "chineseName": "拉基蒂奇", "height": "180cm", "weight": "72kg",
+                    "birthday": "1988 03-10",
+                    "pic": ["public\\img\\yiwan1.jpg", "public\\img\\yiwan1.jpg", "public\\img\\yiwan1.jpg"]
+                },
+                {
+                    "id": 5, "name": "Sergi Busquets Burgos", "chineseName": "布斯克茨", "height": "189cm", "weight": "73kg",
+                    "birthday": "1988 07-16",
+                    "pic": ["public\\img\\buzi1.jpg", "public\\img\\buzi1.jpg", "public\\img\\buzi1.jpg"]
+                },
+                {
+                    "id": 6, "name": "Dani Alves da Silva", "chineseName": "阿尔维斯", "height": "173cm", "weight": "64kg",
+                    "birthday": "1983 05-06",
+                    "pic": ["public\\img\\weisi1.jpg", "public\\img\\weisi1.jpg", "public\\img\\weisi1.jpg"]
+                },
+                {
+                    "id": 8, "name": "Andres Iniesta", "chineseName": "伊涅斯塔", "height": "170cm", "weight": "65kg",
+                    "birthday": "1984 05-11",
+                    "pic": ["public\\img\\yinie1.jpg", "public\\img\\yinie1.jpg", "public\\img\\yinie1.jpg"]
+                },
+                {
+                    "id": 12, "name": "Rafael Alcantara do Nascimento", "chineseName": "拉菲尼亚", "height": "172cm", "weight": "63kg",
+                    "birthday": "1993 03-12",
+                    "pic": ["public\\img\\lafei1.jpg", "public\\img\\lafei1.jpg", "public\\img\\lafei1.jpg"]
+                },
+                {
+                    "id": 13, "name": "Claudio Bravo", "chineseName": "布拉沃", "height": "185cm", "weight": "83kg",
+                    "birthday": "1983 04-13",
+                    "pic": ["public\\img\\bulawo1.jpg", "public\\img\\bulawo1.jpg", "public\\img\\bulawo1.jpg"]
+                },
             ]);
         }
     }
